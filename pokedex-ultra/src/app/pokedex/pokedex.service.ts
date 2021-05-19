94e8cfd338a47e868c513259073760ce1d3c80b2
David Lazaro Fernandez <davidlazaro20@hotmail.com>
2021-05-18 23:33:53 -0500

    Generations finalizado, avance de pokedex, falta consumir api

---
+++ pokedex-ultra/src/app/pokedex/pokedex.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private http: HttpClient) {
+     
+   }
+
+  getPokemones(id){
+    return  this.http.get<any>(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  }
+}
